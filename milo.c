0aa360830874a5a27b4e0f240ca6895eb71e9e3a
Harjith001 <lrsharjith007@gmail.com>
2025-07-01 15:10:34 +0530

    added reading functionalities in progress

--- milo.c
+++ milo.c
@@ -1,5 +1,9 @@
 /*** includes ***/
 
+#define _DEFAULT_SOURCE
+#define _BSD_SOURCE
+#define _GNU_SOURCE
+
 #include <unistd.h>
 #include <termios.h>
 #include <stdlib.h>
@@ -7,6 +11,7 @@
 #include <stdio.h>
 #include <errno.h>
 #include <sys/ioctl.h>
+#include <sys/types.h>
 #include <string.h>
 
 /*** defines ***/
@@ -14,6 +19,8 @@
 #define MILO_VERSION "0.0.1"
 
 #define CTRL_KEY(k) ((k) & 0x1f)
+
+/*** data ***/
 
 enum editorKey {
   ARROW_LEFT = 1000,
@@ -27,12 +34,17 @@ enum editorKey {
   PAGE_DOWN
 };
 
-/*** data ***/
+typedef struct erow {
+  int size;
+  char *chars;
+} erow;
 
 struct editorConfig {
 	int cx, cy;
 	int screenrows;
 	int screencols;
+  int numrows;
+  erow row;
 	struct termios orig_termios;
 };
 
@@ -147,6 +159,29 @@ int getWindowSize(int *rows, int *cols){
 	}
 }
 
+/*** file i/o ***/
+
+void editorOpen(char *filename) {
+  FILE *fp = fopen(filename, "r");
+  if (!fp) die("fopen");
+  char *line = NULL;
+  size_t linecap = 0;
+  ssize_t linelen;
+  linelen = getline(&line, &linecap, fp);
+  if (linelen != -1) {
+    while (linelen > 0 && (line[linelen - 1] == '\n' ||
+                           line[linelen - 1] == '\r'))
+      linelen--;
+    E.row.size = linelen;
+    E.row.chars = malloc(linelen + 1);
+    memcpy(E.row.chars, line, linelen);
+    E.row.chars[linelen] = '\0';
+    E.numrows = 1;
+  }
+  free(line);
+  fclose(fp);
+}
+
 /*** append buffer ***/
 
 struct abuf {
@@ -171,22 +206,28 @@ void abFree(struct abuf *ab) {
 /*** output ***/
 
 void editorDrawRows(struct abuf *ab) {
-	int y;
+  int y;
   for (y = 0; y < E.screenrows; y++) {
-    if (y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(welcome, sizeof(welcome),
-        "Milo editor -- version %s", MILO_VERSION);
-      if (welcomelen > E.screencols) welcomelen = E.screencols;
-			int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+    if (y >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(welcome, sizeof(welcome),
+          "Milo editor -- version %s", MILO_VERSION);
+        if (welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--) abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
     } else {
-      abAppend(ab, "~", 1);
+      int len = E.row.size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row.chars, len);
     }
     abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
@@ -280,13 +321,17 @@ void editorProcessKeypress() {
 void initEditor() {
 	E.cx = 0;
 	E.cy = 0;
+  E.numrows = 0;
 
 	if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
-int main(){
+int main(int argc, char *argv[]){
 	enableRawMode();
 	initEditor();
+  if (argc >= 2) {
+    editorOpen(argv[1]);
+  }
 
 	while (1){
 		editorRefreshScreen();
